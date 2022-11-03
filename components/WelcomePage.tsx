4250af8b267d5beec731d2a4590a97306ed08233
Anisat Akinbani <52763841+Annysah@users.noreply.github.com>
2022-11-03 11:59:46 -0500

    Update the code repo

--- components/WelcomePage.tsx
+++ components/WelcomePage.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const WelcomePage = () => {
   return (
@@ -27,29 +28,29 @@ const WelcomePage = () => {
             </div>
             <div className="grid grid-cols-5 grid-rows-4 gap-4 md:w-5/12 lg:w-6/12">
               <div className="col-span-2 row-span-4">
-                <img
-                  src="https://images.unsplash.com/photo-1644979938680-a3794d2a7bbf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZnJlZSUyMHBsYW50JTIwaW4lMjBhJTIwdmFzZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+                <Image
                   className="rounded-full"
-                  width="640"
-                  height="960"
+                  width={640}
+                  height={960}
+                  src="https://res.cloudinary.com/annysah/image/upload/v1667494587/photo-1644979938680-a3794d2a7bbf_fn2kl0.jpg"
                   alt="plant"
                 />
               </div>
               <div className="col-span-2 row-span-2">
-                <img
-                  src="https://media.istockphoto.com/photos/summer-terrace-with-separate-gazebos-on-the-beach-picture-id1373008572?b=1&k=20&m=1373008572&s=170667a&w=0&h=Qiwj4N39FZ4L8j2cKXD7jKlPEjy_sCCG51_mxDWaiis="
+                <Image
                   className="w-full h-full object-cover object-top rounded-xl"
-                  width="640"
-                  height="640"
+                  width={640}
+                  height={640}
+                  src="https://res.cloudinary.com/annysah/image/upload/v1667494556/summer-terrace-with-separate-gazebos-on-the-beach-picture-id1373008572_h3pcy3.jpg"
                   alt="plant"
                 />
               </div>
               <div className="col-span-3 row-span-3">
-                <img
-                  src="https://images.unsplash.com/photo-1632446087106-eea3332a7bb4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDJ8fGZyZWUlMjBwbGFudCUyMGluJTIwYSUyMHZhc2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60"
+                <Image
                   className="w-full h-full object-cover object-top rounded-xl"
-                  width="640"
-                  height="427"
+                  width={640}
+                  height={427}
+                  src="https://res.cloudinary.com/annysah/image/upload/v1667494529/photo-1632446087106-eea3332a7bb4_teccp2.jpg"
                   alt="plant"
                 />
               </div>
